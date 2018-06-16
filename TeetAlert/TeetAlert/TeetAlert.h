7dd3a9fc83446fb97366983864b032c1493d3dc0
teetbox <teetbox@gmail.com>
2018-06-16 18:35:33 +0800

    Add TeetAlert

---
+++ TeetAlert/TeetAlert/TeetAlert.h
@@ -0,0 +1,19 @@
+//
+//  TeetAlert.h
+//  TeetAlert
+//
+//  Created by Tong Tian on 2018/6/16.
+//  Copyright © 2018 TeetBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeetAlert.
+FOUNDATION_EXPORT double TeetAlertVersionNumber;
+
+//! Project version string for TeetAlert.
+FOUNDATION_EXPORT const unsigned char TeetAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeetAlert/PublicHeader.h>
+
+
